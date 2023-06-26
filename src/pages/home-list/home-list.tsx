5a5ad4a928c689105111e712de0e8fd8510e7525
pablobeernardo <pablobeernardo@gmail.com>
2023-06-26 19:07:43 -0300

    criando pages

---
+++ src/pages/home-list/home-list.tsx
@@ -0,0 +1,18 @@
+import { View, StyleSheet, Text, StatusBar } from "react-native";
+
+export default function HomeList() {
+    return (
+        <View>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
